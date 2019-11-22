9ad457d6bab60dc2be38d40da70bddd0582ca5ca
thenawazrahman <55765671+thenawazrahman@users.noreply.github.com>
2019-11-22 00:03:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,19 @@ io.on('connection', function(socket){
             }
         });
     });
+	
+	//Query to get details from cart
+	socket.on('getCart', function (userID) {
+		database.query("Select userID, c.ISBN, quantity, coverURL, price From cart as c join books as b on c.ISBN Where userID = '" + userID + "'", function (error, results, fields) {
+			if (error) {
+				console.error(error);
+			}
+			else {
+				console.log(results);
+				socket.emit('cartDetails', results[0]);
+			}
+		});
+	});
 
     socket.on('bookRating', function (ISBN) {
         database.query("SELECT AVG(stars) AS rating FROM ratings WHERE ISBN = " + ISBN, function (error, rating, fields) {
