2403986d8134f42e864d65b139ad035af2d2f6d4
Jose Mercado <32403443+jmercado101@users.noreply.github.com>
2019-11-22 18:17:40 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -20,23 +20,148 @@ io.on('connection', function(socket){
     console.log('A user has connected');
 
   //On Disconnection
-    socket.on('disconnect', function(){
-  });
+    socket.on('disconnect', function () {
+    });
 
-  //On Test Event
-  socket.emit('server found','it was found');
+    //On Test Event
+    socket.emit('server found', 'it was found');
 
-  //On Search
-  socket.on('search database', function(search_key){
-		database.query("SELECT title, OnHand, branchName, authorFirst, authorLast, sequence ,publisherName  FROM book as b, inventory as i, branch as br, author as a, publisher as p, wrote as w WHERE b.title = '" + search_key +"' AND b.bookCode = i.bookCode and i.branchNum = br.branchNum and b.bookCode = w.bookCode and w.authorNum = a.authorNum and b.publisherCode = p.publisherCode", function (error, results, fields) {
+
+    //NEW CODE Book Details
+    socket.on('bookDetails', function (ISBN) {
+        if (ISBN != null) {
+            database.query("SELECT books.ISBN, title, genere, coverURL, price, publisher, descript, concat(fName, \" \", lName) AS authorName, bio, author_ID FROM books JOIN author ON author = author_ID WHERE ISBN = '" + ISBN + "'", function (error, results, fields) {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    console.log(results);
+                    socket.emit('detailsResult', results[0]);
+                }
+            });
+        }
+        else {
+            console.error("ERROR: bookDetails: ISBN == null");
+        }
+        
+    });
+	
+	//Query to get details from cart
+	socket.on('getCart', function (userID) {
+        if (userID != null) {
+            database.query("Select userID, c.ISBN, quantity, coverURL, price From cart as c join books as b on c.ISBN Where userID = '" + userID + "'", function (error, results, fields) {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    console.log(results);
+                    socket.emit('cartDetails', results[0]);
+                }
+            });
+        }
+        else {
+            console.error("ERROR: getCart: userID == null");
+        }
+	});
+
+    socket.on('bookRating', function (ISBN) {
+        if (ISBN != null) {
+            database.query("SELECT AVG(stars) AS rating FROM ratings WHERE ISBN = " + ISBN, function (error, rating, fields) {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    if (rating[0].rating != null) {
+                        console.log(rating);
+                        socket.emit('ratingResult', rating[0].rating.toFixed(1));
+                    }
+                    else {
+                        console.log("NOTE: bookRating: No ratings yet.  Book rating returned as 0.");
+                        socket.emit('ratingResult', 0);
+                    }
+                }
+            });
+        }
+        else {
+            console.error("ERROR: bookRating: ISBN == null");
+        }
+    });
+
+    socket.on('testFunction', function (comment) {
+        console.log("testFunctionSays: " + comment);
+    });
+
+    socket.on('purchased', function (ISBN, ID) {
+        if (ISBN != null || ID != null) {
+            database.query("SELECT count(*) AS count FROM purchased WHERE EXISTS (SELECT * FROM purchased WHERE purchased.ID = " + ID + " AND purchased.ISBN = " + ISBN + ")", function (error, purchased, fields) {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    console.log('Book purchased: ' + purchased[0].count);
+                    socket.emit('wasPurchased', purchased[0].count);
+                }
+            });
+        }
+        else {
+            console.error("ERROR: purchased: ISBN or ID == null");
+        }
+    });
+
+    socket.on('comments', function (ISBN) {
+        var maxComments = 100;
+        if (ISBN != null) {
+            database.query("SELECT nickname, anonymity, comments, stars FROM ratings INNER JOIN users ON userID = ID WHERE ISBN =" + ISBN + " LIMIT 0, " + maxComments, function (error, comments, fields) {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    for (i in comments) {
+                        if (comments[i].anonymity == 1) {
+                            console.log("User requested anonymity.\nHiding name: " + comments[i].nickname);
+                            comments[i].nickname = "Anonymous";
+                        }
+                    }
+                    console.log(comments);
+                    socket.emit('gotComments', comments);
+                }
+            });
+        }
+        else {
+            console.error("ERROR: comments: ISBN == null");
+        }
+    });
+
+    // END NEW CODE
+
+
+    //socket on add ratings NEW
+    socket.on('addRatings', function (payload) {
+        var query = "INSERT INTO ratings VALUES('" + payload.ISBN + "','" + payload.user_id + "','" + payload.comment + "', '" + payload.rate + "', '" + payload.anonymity + "')"; //build query, all fields are necessary in add page
+        database.query(query, function (error, results, fields) {
+            if (error) {
+                console.error('Bad query on RATING: ' + error);
+                console.log("Comment on " + payload.ISBN + " failed.");
+            }
+            else {
+                console.log("Added Comment to: " + payload.ISBN);
+            }
+            console.log(results);
+        });
+    });
+    //End NEW
+
+    //On Search FIXED 
+    socket.on('search database', function(search_key){
+		database.query("SELECT title, fName, lName ,publisher, genere, coverURL, price, descript, comments, stars FROM books as b, ratings as r, author as a WHERE (b.title = '" + search_key +"' OR a.fName = '"+search_key+"' OR a.lName = '"+search_key+"' OR b.genere = '"+search_key+"') AND b.author_ID = a.author and b.ISBN = r.ISBN", function (error, results, fields) {
 		if (error) throw error;
 		console.log(results);
-		socket.emit('table result', results);
-	});
-  });
+            socket.emit('table result', results);
+        });
+    });
 
-  //On table request
-  socket.on('table request', function(table){
+    //On table request
+    socket.on('table request', function(table){
 	console.log('Received request for ' + table);
 	 database.query('SELECT * FROM ' + table, function (error, results, fields) {
 		if (error) throw error;
@@ -44,8 +169,8 @@ io.on('connection', function(socket){
 		socket.emit('table result', results);
 	});
   });
-  //on delete from author
-  socket.on('delete author', function(payload){
+    //on delete from author
+    socket.on('delete author', function(payload){
 	var query = "DELETE FROM author where"; //used for building query
 	var a = false;//flags
 	var b = false;
@@ -80,8 +205,8 @@ io.on('connection', function(socket){
 			});
 	}
   });
-  //socket on add author
-  socket.on('add author', function(payload){
+    //socket on add author
+    socket.on('add author', function(payload){
 	var query = "INSERT INTO author VALUES('" + payload.authorNum + "','"+ payload.authorLast + "','" + payload.authorFirst + "')";//build query, all fields are necessary in add page
 		database.query(query, function (error, results, fields) {
 			if (error) throw error;
@@ -89,8 +214,8 @@ io.on('connection', function(socket){
 			socket.emit('table result', results);
 			});
   });
-  //socket on update author
-  socket.on('update author', function(payload){
+    //socket on update author
+    socket.on('update author', function(payload){
 	var query1 = "UPDATE author SET ";//build SET clause
 	var query2 = "WHERE ";//build WHERE clause
 	var a1 = false;
@@ -152,8 +277,8 @@ io.on('connection', function(socket){
 			});
 	
   });
-  //Rest of the sockets follow the same logic as the previous ones
-  socket.on('delete publisher', function(payload){
+    //Rest of the sockets follow the same logic as the previous ones
+    socket.on('delete publisher', function (payload) {
 	var query = "DELETE FROM publisher where";
 	var a = false;
 	var b = false;
@@ -189,7 +314,7 @@ io.on('connection', function(socket){
 	}
   });
 
-   socket.on('add publisher', function(payload){
+    socket.on('add publisher', function (payload) {
 	var query = "INSERT INTO publisher VALUES('" + payload.publisherCode + "','"+ payload.publisherName + "','" + payload.city + "')";
 		database.query(query, function (error, results, fields) {
 			if (error) throw error;
@@ -261,7 +386,7 @@ io.on('connection', function(socket){
 	
   });
 
-  socket.on('delete book', function(payload){
+    socket.on('delete book', function(payload){
 	var query = "DELETE FROM book where";
 	var a = false;
 	var b = false;
@@ -315,7 +440,7 @@ io.on('connection', function(socket){
 	}
   });
 
-  socket.on('add book', function(payload){
+    socket.on('add book', function(payload){
 	var query = "INSERT INTO book VALUES('" + payload.bookCode + "','"+ payload.title + "','" + payload.publisherCode + "','" + payload.type + "','" + payload.paperback + "')";
 		database.query(query, function (error, results, fields) {
 			if (error) throw error;
@@ -324,7 +449,7 @@ io.on('connection', function(socket){
 			});
   });
 
-  socket.on('update book', function(payload){
+    socket.on('update book', function(payload){
 	var query1 = "UPDATE book SET ";
 	var query2 = "WHERE ";
 	var a1 = false;
@@ -423,7 +548,7 @@ io.on('connection', function(socket){
 
   });
 
-  socket.on('delete copy', function(payload){
+    socket.on('delete copy', function(payload){
 	var query = "DELETE FROM copy where";
 	var a = false;
 	var b = false;
@@ -477,7 +602,7 @@ io.on('connection', function(socket){
 	}
   });
 
-  socket.on('add copy', function(payload){
+    socket.on('add copy', function(payload){
 	var query = "INSERT INTO copy VALUES('" + payload.bookCode + "','"+ payload.branchNum + "','" + payload.copyNum + "','" + payload.quality + "'," + payload.price + ")";
 		database.query(query, function (error, results, fields) {
 			if (error) throw error;
@@ -486,7 +611,7 @@ io.on('connection', function(socket){
 			});
   });
 
-  socket.on('update copy', function(payload){
+    socket.on('update copy', function(payload){
 	var query1 = "UPDATE copy SET ";
 	var query2 = "WHERE ";
 	var a1 = false;
