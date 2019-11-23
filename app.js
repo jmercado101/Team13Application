325e91afdd7f5dfc2e09b191959a5f1f0da9318f
Jose Mercado <32403443+jmercado101@users.noreply.github.com>
2019-11-23 11:47:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //Inital Variables
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const crypto = require('crypto');
 var path = require('path');
 
 //EXPRESS
@@ -48,8 +49,8 @@ io.on('connection', function(socket){
 	
 	//Query to get details from cart
 	socket.on('getCart', function (userID) {
-        if (userID != null) {
-            database.query("Select userID, c.ISBN, quantity, coverURL, price From cart as c join books as b on c.ISBN Where userID = '" + userID + "'", function (error, results, fields) {
+        if (userID != null) {	
+		database.query("Select c.userID, c.ISBN, c.quantity, b.coverURL, b.price, b.title From cart as c join books as b on c.ISBN = b.ISBN Where userID = " + userID + ";", function (error, results, fields) {
                 if (error) {
                     console.error(error);
                 }
@@ -161,7 +162,7 @@ io.on('connection', function(socket){
     });
 
     socket.on('purchased', function (ISBN, ID) {
-        if (ISBN != null || ID != null) {
+        if (ISBN != null && ID != null) {
             database.query("SELECT count(*) AS count FROM purchased WHERE EXISTS (SELECT * FROM purchased WHERE purchased.ID = " + ID + " AND purchased.ISBN = " + ISBN + ")", function (error, purchased, fields) {
                 if (error) {
                     console.error(error);
@@ -202,25 +203,57 @@ io.on('connection', function(socket){
     });
 
     // END NEW CODE
-
-
-    //socket on add ratings NEW
-    socket.on('addRatings', function (payload) {
-        var query = "INSERT INTO ratings VALUES('" + payload.ISBN + "','" + payload.user_id + "','" + payload.comment + "', '" + payload.rate + "', '" + payload.anonymity + "')"; //build query, all fields are necessary in add page
-        database.query(query, function (error, results, fields) {
+		
+     socket.on('addUser', function (payload) {
+         var checkDuplicate = "SELECT * FROM users WHERE (users.email = '" + payload.email.toLowerCase() + "'OR users.nickname = '" + payload.nickname + "')";
+        database.query(checkDuplicate, function (error, results, fields) {
             if (error) {
-                console.error('Bad query on RATING: ' + error);
-                console.log("Comment on " + payload.ISBN + " failed.");
+                console.log("An error has occurred");
+                payload.log = "false";
+                socket.emit('userResult', payload);
+            }
+            if (results.length) {
+                console.log("Username or Email taken");
+                payload.log = "false";
+                socket.emit('userResult', payload);
             }
             else {
-                console.log("Added Comment to: " + payload.ISBN);
+                console.log("The username and email will be registered");
+                var query = "INSERT INTO users (fName, lName, email, nickname, passwd) VALUES('" + payload.fName + "','" + payload.lName + "','" + payload.email.toLowerCase() + "','" + payload.nickname + "','" + hashPassword(payload.passwd) + "')";//build query, all fields are necessary in add page		
+                database.query(query, function (error, results, fields) {
+                    if (error) throw error;
+                    console.log(results);
+                    socket.emit('table result', results);
+                });
+                payload.log = "true";
+                socket.emit('userResult', payload);
             }
-            console.log(results);
-        });
+        })
+    });
+	
+    //socket on add ratings NEW
+    socket.on('addRatings', function (token, payload) {
+        var userID = payload.user_id;
+        if (checkToken(token, userID)) {
+            var query = "INSERT INTO ratings VALUES('" + payload.ISBN + "','" + payload.user_id + "','" + payload.comment + "', '" + payload.rate + "', '" + payload.anonymity + "')"; //build query, all fields are necessary in add page
+            database.query(query, function (error, results, fields) {
+                if (error) {
+                    console.error('Bad query on RATING: ' + error);
+                    console.log("Comment on " + payload.ISBN + " failed.");
+                }
+                else {
+                    console.log("Added Comment to: " + payload.ISBN);
+                }
+                console.log(results);
+            });
+        }
+        else {
+            console.error("Invalid token from 'user': " + userID);
+        }
     });
     //End NEW
 
-	//author search
+    //author search
 	socket.on('author search', function(author_ID){
 		database.query("SELECT title, genere, coverURL, price, descript, stars FROM books as b, ratings as r, author as a WHERE b.author_ID = '"+author_ID+"' AND a.author = b.author_ID and b.ISBN = r.ISBN ", function (error, results, fields){
 			if(error) throw error;
@@ -234,9 +267,8 @@ io.on('connection', function(socket){
 		database.query("SELECT title, fName, lName ,publisher, genere, coverURL, price, descript, comments, stars FROM books as b, ratings as r, author as a WHERE (b.title = '" + search_key +"' OR a.fName = '"+search_key+"' OR a.lName = '"+search_key+"' OR b.genere = '"+search_key+"') AND b.author_ID = a.author and b.ISBN = r.ISBN", function (error, results, fields) {
 		if (error) throw error;
 		console.log(results);
-        socket.emit('table result', results);
+            socket.emit('table result', results);
         });
-		
     });
 
     //On table request
@@ -247,11 +279,65 @@ io.on('connection', function(socket){
 		console.log(results);
 		socket.emit('table result', results);
 	});
-  });
+    });
+
+    //Sign In check
+    socket.on('signIn', function (email, password) {
+        email = email.toLowerCase();
+        if (email != null && password != null) {
+            console.log('Attemted sign in from: ' + email);
+            console.log(password)
+            database.query("SELECT * FROM users WHERE email = '" + email + "'", function (error, users, fields) {
+                if(error || users[0] == null)
+                {
+                    console.error(error);
+                }
+                else
+                {
+                    if (hashPassword(password) == users[0].passwd) {
+                        console.log("Success!")
+                        socket.emit('authenticated', generateToken(users[0].passwd, users[0].email, users[0].ID), users[0].ID);
+                    }
+                    else {
+                        console.log("Failure.  Invalid Login.")
+
+                        console.log("Password mismatch:");
+                        console.log(hashPassword(password));
+                        console.log(users[0].passwd);
+                        socket.emit('authenticated', "invalidLogin", null);
+                    }
+                }
+                
+            });
+        }
+    });
+
+    function generateToken(hashedPassword, email, userID) {
+        return crypto.createHash('sha512').update(hashedPassword + email.toLowerCase() + userID).digest('hex');
+    }
+    function hashPassword(inPassword) {
+        return crypto.createHash('sha512').update(inPassword).digest('hex');
+    }
+    function checkToken(providedToken, userID)
+    {
+        if (providedToken != null && userID != null) {
+            return database.query("SELECT * FROM users WHERE ID = '" + userID + "'", function (error, user, fields) {
+                if (error) {
+                    console.error(error);
+                    return false;
+                }
+                else {
+                    var knownGoodToken = generateToken(user[0].passwd, user[0].email, user[0].ID);
+                    if (providedToken == knownGoodToken) {
+                        console.log("User " + userID + " authenticated.");
+                        return true;
+                    }
+                }
+            });
+        }
+    }
  
 
 });
 //database connection
 var database = require('./database.js');
-
-
